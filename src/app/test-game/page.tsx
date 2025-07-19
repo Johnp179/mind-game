50e3b8cd1f51c0a5092463fb603201c9232000b2
searai <johnpoconnor4@gmail.com>
2025-07-19 14:19:03 +0100

    changed the test file

--- src/app/test-game/page.tsx
+++ src/app/test-game/page.tsx
@@ -1,40 +1,13 @@
 "use client";
 
-import { useState, useEffect } from "react";
-
-function useInitializer(id: string) {
-  useEffect(() => {
-    console.log(`comp ${id} has just been created`);
-    return () => console.log(`comp ${id} is now destroyed`);
-  }, []);
-}
-
-function CompTwo({ switcher }: { switcher: () => void }) {
-  useInitializer("2");
-  return (
-    <div>
-      <h1>This is the the second Component</h1>
-      <button onClick={() => switcher()}>switch</button>
-    </div>
-  );
-}
-function CompOne({ switcher }: { switcher: () => void }) {
-  useInitializer("1");
-  return (
-    <div>
-      <h1>This is the first Component</h1>
-      <button onClick={switcher}>switch</button>
-    </div>
-  );
+function doSomething() {
+  return "this is just some random data";
 }
 
 export default function Page() {
-  const [switcher, setSwitcher] = useState(false);
-
-  function onClick() {
-    setSwitcher(!switcher);
-  }
-
-  if (switcher) return <CompOne switcher={onClick} />;
-  return <CompTwo switcher={onClick} />;
+  return (
+    <div>
+      <h1>{doSomething()}</h1>
+    </div>
+  );
 }
